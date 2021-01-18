3f271cdb942b6539ec170acd91d5256b31a9a304
Jason-Fyfe <JFyfe@academytrainee.com>
2021-01-18 12:27:39 +0000

    Added REST annotation

--- src/main/java/com/qa/choonz/persistence/repository/AlbumRepository.java
+++ src/main/java/com/qa/choonz/persistence/repository/AlbumRepository.java
@@ -1,11 +1,11 @@
 package com.qa.choonz.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.qa.choonz.persistence.domain.Album;
 
-@Repository
+@RepositoryRestResource
 public interface AlbumRepository extends JpaRepository<Album, Long> {
 
 }
