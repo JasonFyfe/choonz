2633199977e86fc0a7c9d41ced4c87ab3b0c83f7
Jason-Fyfe <JFyfe@academytrainee.com>
2021-01-18 17:04:01 +0000

    No ID constructor added to Artist

--- src/main/java/com/qa/choonz/config/AppConfig.java
+++ src/main/java/com/qa/choonz/config/AppConfig.java
@@ -10,6 +10,17 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.qa.choonz.persistence.domain.Artist;
+import com.qa.choonz.persistence.domain.Album;
+import com.qa.choonz.persistence.domain.Track;
+import com.qa.choonz.persistence.domain.Genre;
+import com.qa.choonz.persistence.domain.Playlist;
+import com.qa.choonz.persistence.repository.AlbumRepository;
+import com.qa.choonz.persistence.repository.ArtistRepository;
+import com.qa.choonz.persistence.repository.TrackRepository;
+import com.qa.choonz.persistence.repository.GenreRepository;
+import com.qa.choonz.persistence.repository.PlaylistRepository;
+
 @Configuration
 public class AppConfig {
 	
@@ -32,8 +43,20 @@ public class AppConfig {
     }
     
     @Bean
-    CommandLineRunner initDatabase() {
-    	return args -> {
+    CommandLineRunner initDatabase(ArtistRepository artistRepo,
+    							   AlbumRepository albumRepo,
+    							   TrackRepository trackRepo,
+    							   GenreRepository genreRepo,
+    							   PlaylistRepository playlistRepo)
+    {
+    	return args ->
+    	{
+    		// Artists
+    		log.info("Preloading " + artistRepo.save(new Artist("The Mountain Goats")));
+    		log.info("Preloading " + artistRepo.save(new Artist("We Were Promised Jetpacks")));
+    		// Albums
+    		log.info("Preloading " + albumRepo.save(new Album("The Mountain Goats")));
+    		log.info("Preloading " + albumRepo.save(new Album("We Were Promised Jetpacks")));
     	};
     }
 
