bf157a2f2f5455393be4711a1dd9a7e52f64b7b9
Malik <malik_ali1998@hotmail.co.uk>
2021-01-19 10:56:41 +0000

    unit testing for artist controller complete

--- src/main/java/com/qa/choonz/rest/controller/GenreController.java
+++ src/main/java/com/qa/choonz/rest/controller/GenreController.java
@@ -22,37 +22,37 @@ import com.qa.choonz.service.GenreService;
 @CrossOrigin
 public class GenreController {
 
-    private GenreService service;
+	private GenreService service;
 
-    public GenreController(GenreService service) {
-        super();
-        this.service = service;
-    }
+	public GenreController(GenreService service) {
+		super();
+		this.service = service;
+	}
 
-//    @PostMapping("/create")
-//    public ResponseEntity<GenreDTO> create(@RequestBody Genre genre) {
-//        return new ResponseEntity<GenreDTO>(this.service.create(genre), HttpStatus.CREATED);
-//    }
-//
-//    @GetMapping("/read")
-//    public ResponseEntity<List<GenreDTO>> read() {
-//        return new ResponseEntity<List<GenreDTO>>(this.service.read(), HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/read/{id}")
-//    public ResponseEntity<GenreDTO> read(@PathVariable long id) {
-//        return new ResponseEntity<GenreDTO>(this.service.read(id), HttpStatus.OK);
-//    }
-//
-//    @PostMapping("/update/{id}")
-//    public ResponseEntity<GenreDTO> update(@RequestBody Genre genre, @PathVariable long id) {
-//        return new ResponseEntity<GenreDTO>(this.service.update(genre, id), HttpStatus.ACCEPTED);
-//    }
-//
-//    @DeleteMapping("/delete/{id}")
-//    public ResponseEntity<GenreDTO> delete(@PathVariable long id) {
-//        return this.service.delete(id) ? new ResponseEntity<GenreDTO>(HttpStatus.NO_CONTENT)
-//                : new ResponseEntity<GenreDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
+	@PostMapping("/create")
+	public ResponseEntity<GenreDTO> create(@RequestBody Genre genre) {
+		return new ResponseEntity<GenreDTO>(this.service.create(genre), HttpStatus.CREATED);
+	}
+
+	@GetMapping("/read")
+	public ResponseEntity<List<GenreDTO>> read() {
+		return new ResponseEntity<List<GenreDTO>>(this.service.read(), HttpStatus.OK);
+	}
+
+	@GetMapping("/read/{id}")
+	public ResponseEntity<GenreDTO> read(@PathVariable long id) {
+		return new ResponseEntity<GenreDTO>(this.service.read(id), HttpStatus.OK);
+	}
+
+	@PostMapping("/update/{id}")
+	public ResponseEntity<GenreDTO> update(@RequestBody Genre genre, @PathVariable long id) {
+		return new ResponseEntity<GenreDTO>(this.service.update(genre, id), HttpStatus.ACCEPTED);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<GenreDTO> delete(@PathVariable long id) {
+		return this.service.delete(id) ? new ResponseEntity<GenreDTO>(HttpStatus.NO_CONTENT)
+				: new ResponseEntity<GenreDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 
 }
