63ac3fda027f61e8379bc5784b36a3970fca6da1
Malik <malik_ali1998@hotmail.co.uk>
2021-01-25 23:33:54 +0000

    update functionality for album

--- src/main/resources/static/js/album.js
+++ src/main/resources/static/js/album.js
@@ -1,9 +1,9 @@
 const params = new URLSearchParams(window.location.search);
 
 for (let param of params) {
-    console.log("here i am", param)
+    //console.log("here i am", param)
     let id = param[1];
-    console.log(id);
+    //console.log(id);
     getData(id)
 }
 
@@ -17,17 +17,20 @@ function getData(id) {
                     return;
                 }
                 response.json().then(function (data) {
-                    console.log(data);
+                    //console.log(data);
+                    
+                    document.querySelector("input#id").value = data.id;
+                  
+                    document.querySelector("input#name").value = data.name;
+                    document.querySelector("input#cover").value = data.cover;
 
-
-
-                    document.getElementById("main").innerHTML = `
+                document.getElementById("main").innerHTML = `
                 <div class ="albumone">
                 <h1>${data.id}</h1>    
                 <h2>${data.name}</h2>
                 <h4>Tracks: ${data.tracks}</h4>
                 <p>Cover: ${data.cover}</p>
-                <button onclick="remove(${data.id})">Delete</button>
+                <button onclick="deleteByid(${data.id})">Delete</button>
                 
                 </div> 
                 
@@ -41,6 +44,49 @@ function getData(id) {
 }
 
 
+    document.getElementById("update").onclick = function() {myFunction()};
+
+    function myFunction() {
+        var id = document.querySelector('#id').value;
+        var namealbum = document.querySelector('#name');
+        var cover = document.querySelector('#cover');
+
+
+    
+        let data = {
+            "name" :  namealbum.value,
+            "cover": cover.value
+
+
+              
+          }
+          console.log("Data to post",data)   
+          console.log(id)     
+            console.log(namealbum.value);
+            console.log(cover.value);
+            console.log("Data to post",data)
+            sendData(data, id)
+            
+  
+      }
+
+      function sendData(data, id){
+        fetch("http://localhost:8082/albums/"+ id, {
+            method: 'put',
+            headers: {
+              "Content-type": "application/json; charset=UTF-8"
+            },
+            body:JSON.stringify(data)
+          })
+          .then(function (data) {
+            console.log('Request succeeded with JSON response', data);
+          })
+          .catch(function (error) {
+            console.log('Request failed', error);
+          });
+          //location.reload()
+        }
+
 function deleteByid(id) {
     fetch("http://localhost:8082/albums/" + id, {
         method: 'delete',
@@ -51,4 +97,3 @@ function deleteByid(id) {
 
 
 }
-
